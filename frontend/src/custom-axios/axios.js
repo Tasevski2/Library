d01242e272db4aedadaefc1ff394c6cd7aeda3df
Viktor Tasevski <viktor-tasevski@hotmail.com>
2022-04-25 14:44:44 +0200

    backend url

--- frontend/src/custom-axios/axios.js
+++ frontend/src/custom-axios/axios.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://emtlab2backend193018.herokuapp.com',
+    baseURL: 'https://emtlibrarybackend191149.herokuapp.com',
     headers: {
-        'Access-Control-Allow-Origin': '*'
-    }
-})
+        'Access-Control-Allow-Origin': '*',
+    },
+});
 
 export default instance;
